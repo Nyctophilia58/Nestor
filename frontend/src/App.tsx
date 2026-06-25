aca0c44abee94a1cb4cc6f32ffb398e394c4f9c9
Homiya Nowshin <nowshingdr@protonmail.com>
2026-06-25 12:21:36 +0600

    feat: app routing frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,25 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Listings from "./pages/Listings";
+import PropertyDetail from "./pages/PropertyDetail";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AddProperty from "./pages/AddProperty";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <>
-      <h1>Hello!!!</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/listings" element={<Listings />} />
+        <Route path="/listings/:id" element={<PropertyDetail />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/add-property" element={<AddProperty />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
